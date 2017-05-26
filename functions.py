1dfda3722df719f007d30fba373177d7475c0ba4
mtoto <tszilagyi@outlook.com>
2017-05-26 08:50:20 +0200

    parser functions and luigi ETL tasks

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import requests
 import datetime
 import json
+import collections
+
 from spotify_creds import *
 
 """ Get acces token """
@@ -37,5 +39,71 @@ def download_data():
         json.dump(data['items'], f)
         f.write('\n')
     
+""" Convert json to a list of dicts"""
+def json_parser(file):
+    result =[]
+    with open(file) as f:
+        for line in f:
+            data = json.loads(line)
+            result.extend(data)
+    return(result)
+
+"""Parse json so it can easily be converted to a dataframe"""
+def parse_json(file): 
+    
+    dict_list = json_parser(file)
+
+    results = []
+    track_cols = ['name','uri','explicit','preview_url',
+                  'track_number','disc_number','href',
+                  'duration_ms','type','id']
+
+    for item in dict_list:
+        
+        d_time = {'played_at' : item['played_at'] }
+        
+        if item['context'] is not None:
+            
+            d_context = item['context'] 
+            d_context['spotify_external_url'] = item['context']['external_urls']['spotify']
+            d_context['playlist_href'] = item['context']['href'] # deal with other duplicate names
+            d_context.pop('external_urls', None)
+            
+        else:
+            d_context = {'href' : None, 'spotify_external_url': None,
+                         'type' : None, 'uri' : None, 'playlist_href': None}
+
+        
+        for key in item.keys():
+            
+            if (key == 'track'):
+              
+                track = item[key]
+                d_arts = collections.defaultdict(list)
+                
+                for i in track['artists']:
+                    for k, v in i.items():
+                        
+                        if (k != 'external_urls'):
+                            d_arts[k].append(v)
+                            
+                        elif (k == 'external_urls'):
+                            d_arts['artist_urls'].append(v['spotify'])
+                                    
+                track_sub = { k: track[k] for k in track_cols }     
+                track_sub['track_name'] = track_sub.pop('name')
+            
+
+        d = dict(track_sub, **d_arts)
+        d.update(d_time)
+        d.update(d_context)
+
+        results.append(d)
+        
+        result = {v['played_at']:v for v in results}.values()
+
+                
+    return(result)
+
 
 
---
+++ tasks.py
@@ -0,0 +1,33 @@
+# Filename: run_luigi.py
+import luigi
+from luigi.s3 import S3Target, S3Client
+from luigi.util import inherits
+import json
+import sys
+from datetime import date, timedelta
+from functions import *
+
+class spotify_local_file(luigi.ExternalTask):
+    date = luigi.DateParameter()
+
+    def output(self):
+        return luigi.LocalTarget("json/spotify_tracks_%s.json" % self.date.strftime('%Y-%m-%d'))
+
+@inherits(spotify_local_file)
+class spotify_clean_aws(luigi.Task):
+    date = luigi.DateParameter(default = date.today()-timedelta(1))
+
+    def requires(self):
+            return self.clone(spotify_local_file)
+
+    def output(self):
+        client = S3Client(host = 's3.us-east-2.amazonaws.com')
+        return S3Target('s3://myspotifydata/spotify_test_%s.json' % self.date.strftime('%Y-%m-%d'), client=client)
+
+    def run(self):
+        data = parse_json("json/spotify_tracks_%s.json" % self.date.strftime('%Y-%m-%d'))
+        with self.output().open('w') as out_file:
+            json.dump(data, out_file)
+
+if __name__ == '__main__':
+    luigi.run()
